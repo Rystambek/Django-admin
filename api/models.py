e159d2b6f99256fb923f3dc2e7c33e3b35995834
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-17 00:57:30 +0500

    kompyuter

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Kompyuter(models.Model):
+    name = models.CharField(max_length=50)
+    brend = models.CharField(max_length=50)
+    price = models.CharField(max_length=10)
+    xotira = models.CharField(max_length=50)
+    protsessor = models.CharField(max_length=50)
+    about = models.TextField()
