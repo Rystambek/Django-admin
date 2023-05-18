1eb871e6a3aa111626621d368206548dfde211f0
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-18 14:16:12 +0500

    delete route

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.views import home,smartphones,add_smartphone,get_id
+from api.views import home,smartphones,add_smartphone,get_id,get_delete
 
 urlpatterns = [
     path('', home),
     path('get/all/',smartphones),
     path('add/',add_smartphone),
-    path('get/<id>',get_id)
+    path('get/<id>',get_id),
+    path('delete/<id>',get_delete)
 ]
