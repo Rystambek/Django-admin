38ffa8fdedc5eab9149b30bcb959ca70b298d19f
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-18 10:54:46 +0500

    smartphone registor

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Kompyuter
+from .models import Kompyuter,Smartphones
 
 admin.site.register(Kompyuter)
+admin.site.register(Smartphones)
