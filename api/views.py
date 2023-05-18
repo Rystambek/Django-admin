51ce2c103b4abc2ad0d823764dec953705d609b2
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-18 11:05:44 +0500

    smartphone func

--- api/views.py
+++ api/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse,JsonResponse
 # Create your views here.
+from .models import Smartphones
 
 def home(request:HttpRequest):
     return HttpResponse("<h1>Hello Tashkent</h1>")
+
+
+def smartphones(request:HttpRequest):
+    smartphone = Smartphones.objects.all()
+
+    results = []
+    for phone in smartphone:
+        results.append({
+            'id':phone.pk,
+            'price':phone.price,
+            'img_url':phone.img_url,
+            'color':phone.color,
+            'ram':phone.ram,
+            'memory':phone.memory,
+            'name':phone.name,
+            'model':phone.model
+        })
+
+    return JsonResponse({'results':results})
