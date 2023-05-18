7a12e836be524c9522db7f4e612f68edd14f8501
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-18 11:16:28 +0500

    add route

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.views import home,smartphones
+from api.views import home,smartphones,add_smartphone
 
 urlpatterns = [
     path('', home),
-    path('smartphone/',smartphones)
+    path('smartphone/',smartphones),
+    path('add/',add_smartphone)
 ]
