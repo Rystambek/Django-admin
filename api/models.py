c44745aa0d269d8168716a109da6f9de03fabd99
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-18 10:47:50 +0500

    smartphone

--- api/models.py
+++ api/models.py
@@ -9,3 +9,16 @@ class Kompyuter(models.Model):
     xotira = models.CharField(max_length=50)
     protsessor = models.CharField(max_length=50)
     about = models.TextField()
+
+
+class Smartphones(models.Model):
+    price = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    ram = models.CharField(max_length=255)
+    memory = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
