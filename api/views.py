3881d75db10b9472006dd177a6fb8b01339d890b
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-19 22:44:06 +0500

    add phone and update

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse,JsonResponse
 # Create your views here.
 from .models import Smartphones
+import json
 
 def home(request:HttpRequest):
     return HttpResponse("<h1>Hello Tashkent</h1>")
@@ -26,25 +27,30 @@ def smartphones(request:HttpRequest):
     return JsonResponse({'results':results})
 
 def add_smartphone(request:HttpRequest):
-    phone = Smartphones(
-        price = "1500000",
-        img_url = 'dfewfafw',
-        color = 'while',
-        ram = '16 GB',
-        memory = '256 GB',
-        name = 'Samsung A22',
-        model = 'Samsung'
-    )
-    phone.save()
+    if request.method == "POST":
+        data = request.body.decode()
+        data_json = json.loads(data)
 
+        phone = Smartphones(
+            price = data_json['price'],
+            img_url = data_json['img_url'],
+            color = data_json['color'],
+            ram = data_json['ram'],
+            memory = data_json['memory'],
+            name = data_json['name'],
+            model = data_json['model']
+        )
+
+        phone.save()
+    
     return JsonResponse({'Status':200})
 
-def get_id(request:HttpRequest,id:int):
+def get_id(request:HttpRequest,id):
     smartphone = Smartphones.objects.all()
 
     result = []
     for phone in smartphone:
-        if int(id) == phone.pk:
+        if id == phone.pk:
             result.append({
             'id':phone.pk,
             'price':phone.price,
@@ -60,10 +66,30 @@ def get_id(request:HttpRequest,id:int):
         return HttpResponse('Bunday IDdagi maxsulot yo\'q')
     
 
-def get_delete(request:HttpRequest,id:int):
+def get_delete(request:HttpRequest,id):
     smartphone = Smartphones.objects.all()
     for phone in smartphone:
-        if int(id) == phone.pk:
+        if id == phone.pk:
             phone.delete()
     return JsonResponse({'status':200})
+
+def get_update(request:HttpRequest,id):
+    if request.method == "POST":
+        data = request.body.decode()
+        data_json = json.loads(data)
+
+        smartphones = Smartphones.objects.all()
+        for phone in smartphones:
+            if id == phone.pk:
+                phone.price = data_json['price']
+                phone.img_url = data_json['img_url']
+                phone.color = data_json['color']
+                phone.ram = data_json['ram']
+                phone.memory = data_json['memory']
+                phone.name = data_json['name']
+                phone.model = data_json['model']
+                
+            phone.save()    
+    return JsonResponse({'Update':200})
+
 
