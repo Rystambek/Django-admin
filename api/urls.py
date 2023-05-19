44239012d21832a825476c5b11aa1a9453701dd3
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-19 22:43:10 +0500

    delete and update

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from api.views import home,smartphones,add_smartphone,get_id,get_delete
+from api.views import home,smartphones,add_smartphone,get_id,get_delete,get_update
 
 urlpatterns = [
     path('', home),
     path('get/all/',smartphones),
     path('add/',add_smartphone),
-    path('get/<id>',get_id),
-    path('delete/<id>',get_delete)
+    path('get/<int:id>',get_id),
+    path('delete/<int:id>',get_delete),
+    path('update/<int:id>',get_update)
 ]
